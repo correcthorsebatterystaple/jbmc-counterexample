7f9318f065c6d65635d626d8cd5b58209d01fde5
Burhanuddin Salim <burhan.salman@gmail.com>
2022-05-10 23:45:47 +0100

    Cleaning up

--- src/cbmc_parser.py
+++ src/cbmc_parser.py
@@ -2,16 +2,11 @@ import xml.etree.ElementTree as ET
 from lxml import etree
 import subprocess
 import sys
+from java_generator import generate_java_source
 
 # Code to generate a java file with inputs for the test function of the supplied class
 def generate_java(filename, counterexample_inputs):
-    code = "class Counterexample { \n \tpublic static void main(String[] args) { \n \t"
-    for input_var in counterexample_inputs:
-        code += f'\t{counterexample_inputs[input_var]["type"]} {input_var} = ' \
-                f'{counterexample_inputs[input_var]["value"]}; \n \t'
-
-    code += f'\t{filename}.test({", ".join(counterexample_inputs.keys())}); \n \t'
-    code += "} \n }"
+    code = generate_java_source(filename, counterexample_inputs)
 
     with open("Counterexample.java", "w") as f:
         f.write(code)
@@ -33,15 +28,10 @@ def compile_and_run_java(jbmc_path, filepath):
 def get_inputs(filename):
     file = open(filename + ".txt", "rb")
     tree = etree.parse(file)
-    print(tree.getroot().tag)
     root = tree.getroot()
-    # print(root.tag)
     for child in root:
-        # print(child.tag, child.attrib)
         if child.tag == 'result' and child.attrib['status'] == 'FAILURE':
 
-            function_name = child.attrib['property'].split('::')[1].split(':')[0]
-            print(child.tag, child.attrib)
             # Get goto_trace
             goto_trace = None
             for child1 in child:
@@ -53,7 +43,6 @@ def get_inputs(filename):
             for goto_trace_child in goto_trace:
                 if goto_trace_child.tag == 'assignment' \
                         and goto_trace_child.attrib['base_name'].startswith('arg'):
-                    # print(goto_trace_child.attrib['base_name'])
                     value_type = None
                     actual_value = None
                     for attribute in goto_trace_child:
@@ -63,15 +52,12 @@ def get_inputs(filename):
                         if attribute.tag == 'full_lhs_value':
                             actual_value = attribute.text
 
-                    # print(goto_trace_child.attrib['base_name'], value_type, actual_value)
                     if goto_trace_child.attrib['base_name'] not in inputs_list:
                         inputs_list[goto_trace_child.attrib['base_name']] = {}
 
                     inputs_list[goto_trace_child.attrib['base_name']]['type'] = value_type
                     inputs_list[goto_trace_child.attrib['base_name']]['value'] = actual_value
 
-            print(function_name)
-            print(inputs_list)
             return inputs_list
 
 jbmc_path = sys.argv[1]
---
+++ src/java_generator.py
@@ -0,0 +1,20 @@
+def generate_java_source(class_name: str, counterexample_inputs) -> str:
+    """Generate java souce code from counter example inputs"""
+    source_builder = []
+    source_builder.append('class Counterexample {')
+    source_builder.append('\tpublic static void main(String[] args) {')
+    
+    for input_var in counterexample_inputs:
+        input_type = counterexample_inputs[input_var]['type']
+        input_value = counterexample_inputs[input_var]['value']
+        lhs = f"{input_type} {input_var}"
+        rhs = f"{input_value}"
+        expr = f"{lhs} = {rhs}"
+        source_builder.append(f"\t{expr}")
+
+    arg_list = ", ".join(counterexample_inputs.keys())
+    source_builder.append(f'\t{class_name}.test({arg_list});')
+    source_builder.append('\t}')
+    source_builder.append('}')
+
+    return '\n'.join(source_builder)
