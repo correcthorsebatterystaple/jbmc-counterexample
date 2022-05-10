a2017d1400c0bf4ac87ee9d3901f143a7bb3c6d9
Burhanuddin Salim <burhan.salman@gmail.com>
2022-05-10 20:06:52 +0100

    Add examples for string compare

---
+++ examples/string-compare/Input.java
@@ -0,0 +1,5 @@
+class Input {
+	public static void test(String a, String b) {
+		assert !a.equals(b);
+	}
+}
---
+++ examples/string-compare/Output.java
@@ -0,0 +1,8 @@
+class Output {
+	public static void main(String[] args) {
+		String arg0 = "a";
+		String arg1 = "b";
+
+		Input.test(arg0, arg1);
+	}
+}
