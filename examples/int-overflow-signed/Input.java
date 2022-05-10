965500a819e6b807137fd3dd3cea3f327e3516ff
Burhanuddin Salim <burhan.salman@gmail.com>
2022-05-10 19:53:01 +0100

    Add signed int overflow examples

---
+++ examples/int-overflow-signed/Input.java
@@ -0,0 +1,6 @@
+class Input {
+	public static void test(int x) {
+		int y = x + 1;
+		assert y > x;
+	}
+}
---
+++ examples/int-overflow-signed/Output.java
@@ -0,0 +1,5 @@
+class Output {
+	public static void main(String[] args) {
+		Input.test(21474836478);
+	}
+}
