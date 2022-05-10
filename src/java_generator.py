9bade533161e7884adbac37bb1b4dfc42d3eedc2
Burhanuddin Salim <burhan.salman@gmail.com>
2022-05-11 00:15:35 +0100

    Fix java_generator output formatting

--- src/java_generator.py
+++ src/java_generator.py
@@ -3,17 +3,17 @@ def generate_java_source(class_name: str, counterexample_inputs) -> str:
     source_builder = []
     source_builder.append('class Counterexample {')
     source_builder.append('\tpublic static void main(String[] args) {')
-    
+
     for input_var in counterexample_inputs:
         input_type = counterexample_inputs[input_var]['type']
         input_value = counterexample_inputs[input_var]['value']
         lhs = f"{input_type} {input_var}"
         rhs = f"{input_value}"
         expr = f"{lhs} = {rhs}"
-        source_builder.append(f"\t{expr}")
+        source_builder.append(f"\t\t{expr};")
 
     arg_list = ", ".join(counterexample_inputs.keys())
-    source_builder.append(f'\t{class_name}.test({arg_list});')
+    source_builder.append(f'\t\t{class_name}.test({arg_list});')
     source_builder.append('\t}')
     source_builder.append('}')
 
